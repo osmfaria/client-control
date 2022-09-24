c10dda91545c198327e8b756e8843358ec341fae
Osmar Faria <osm.faria@gmail.com>
2022-09-23 20:35:09 -0700

    port updated

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ import express from "express"
 import appRoutes from "./routes"
 import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware"
 
+const cors = require('cors')
+
+
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 appRoutes(app)
 
